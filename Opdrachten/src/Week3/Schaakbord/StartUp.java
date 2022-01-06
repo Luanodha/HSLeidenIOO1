1ccf6c24faae877fd1410105a3381a38f58e7825
Your Name <you@example.com>
2022-01-06 16:41:27 +0100

    Begin gemaakt aan schaakbord

--- Opdrachten/src/Week3/Schaakbord/StartUp.java
+++ Opdrachten/src/Week3/Schaakbord/StartUp.java
@@ -5,6 +5,6 @@ public class StartUp {
     public static void main(String[] args) {
 
         Schaakbord schaakbord = new Schaakbord();
-        System.out.println(schaakbord.getArrayPosities("A8");
+        //System.out.println(schaakbord.getArrayPosities("A8");
     }
 }
