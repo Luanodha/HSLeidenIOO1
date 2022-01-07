bcbce8c7a817df965c7126ab5668b4b8ce6e8602
Your Name <you@example.com>
2022-01-07 10:12:51 +0100

    t/m garage opdracht

--- Opdrachten/src/Week99/GarageMedewerker.java
+++ Opdrachten/src/Week99/GarageMedewerker.java
@@ -2,5 +2,41 @@ package Week99;
 
 public class GarageMedewerker {
 
-    static String naam;
+    private String naam;
+    private String woonplaats;
+    private int geboortejaar;
+
+    public GarageMedewerker(String naam, String woonplaats, int geboortejaar) {
+        this.naam = naam;
+        this.woonplaats = woonplaats;
+        this.geboortejaar = geboortejaar;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public int getGeboortejaar() {
+        return geboortejaar;
+    }
+
+    public void setGeboortejaar(int geboortejaar) {
+        this.geboortejaar = geboortejaar;
+    }
+
+    public int getLeeftijd(int geboortejaar){
+        return (2021 - geboortejaar);
+    }
 }
